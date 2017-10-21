d8703736241eb9797c4542dfd3b765ef900d3f0e
apolens <poldus1993@gmail.com>
2017-10-21 23:05:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/adrian/aplikacjewielowarstwowe/MobileApp.java
@@ -0,0 +1,13 @@
+package com.example.adrian.aplikacjewielowarstwowe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MobileApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile_app);
+    }
+}
